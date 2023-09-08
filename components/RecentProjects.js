5b51e840ae72d30915c97869ad5c659e28bf6425
luke-meadows <lukemeadowsdev@gmail.com>
2023-09-08 13:06:34 +0100

    change

--- components/RecentProjects.js
+++ components/RecentProjects.js
@@ -3,7 +3,7 @@ import Container from './Container';
 import RecentProjectItem from './RecentProjectItem';
 import ProjectImg1 from '../public/images/recent-project-placeholders/Bickerton Road.jpeg';
 import ProjectImg2 from '../public/images/recent-project-placeholders/19thCentury.jpg';
-// import ProjectImg3 from '../public/images/recent-project-placeholders/House.jpeg';
+import ProjectImg3 from '../public/images/recent-project-placeholders/House.jpeg';
 import ProjectImg4 from '../public/images/recent-project-placeholders/Richmond Road.jpg';
 import ProjectImg5 from '../public/images/recent-project-placeholders/Bryants House.png';
 import ProjectImg6 from '../public/images/recent-project-placeholders/Devonshire.jpeg';
@@ -28,8 +28,8 @@ export default function RecentProjects() {
             img={ProjectImg2}
             title="19th century Victorian property"
           />
-          {/* <RecentProjectItem img={ProjectImg3} title="Archers Road?" /> */}
-          <RecentProjectItem img={ProjectImg4} title="Richmond Road" />
+          <RecentProjectItem img={ProjectImg3} title="Archers Road?" />
+
           <RecentProjectItem img={ProjectImg4} title="Richmond Road" />
           <RecentProjectItem img={ProjectImg5} title="Bryant’s house" />
           <RecentProjectItem img={ProjectImg6} title="Devonshire" />
