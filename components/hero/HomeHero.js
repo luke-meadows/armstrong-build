a376a31dbc367c8ad1297f5bce5a574e45d98383
luke-meadows <lukemeadowsdev@gmail.com>
2023-10-11 13:48:06 +0100

    changes

--- components/hero/HomeHero.js
+++ components/hero/HomeHero.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import Container from '../Container';
 import Image from 'next/image';
-import HeroImg from '../../public/images/hero-images/home-hero.jpeg';
+import HeroImg from '../../public/images/hero-images/home-hero2.jpeg';
 export default function HomeHero() {
   return (
     <StyledHero>
