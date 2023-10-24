323711b8eac39b529f4312f5f301b24cf3a3a057
luke-meadows <lukemeadowsdev@gmail.com>
2023-10-24 12:38:31 +0100

    new background

--- components/Background.js
+++ components/Background.js
@@ -10,6 +10,7 @@ export default function Background() {
           layout="fill"
           objectFit="cover"
           alt="background image"
+          priority
         />
       </div>
     </StyledBackground>
