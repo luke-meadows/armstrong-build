610c1b5e32efd10700b361b2ed6baa86ade80426
luke-meadows <lukemeadowsdev@gmail.com>
2023-12-07 12:50:54 +0000

    faq

--- components/FaqSection.js
+++ components/FaqSection.js
@@ -1,22 +1,48 @@
 import styled from 'styled-components';
 import Container from './Container';
+import FaqItem from './service-page-into-sections/FaqItem';
 export default function FaqSection() {
+  const faqs = [
+    {
+      question: 'What building services do you offer?',
+      answer:
+        'Sure. Arrange a no obligation site visit with our surveyor by calling 0121 441 5222. We can usually call out within 2 working days including Saturday. Site visit takes between 20-30 minutes depending on work required. Our representative will discuss your needs and explain anything you might not be sure about. We draw up a detailed plan of work required and costs breakdown. We will also include a guaranteed start date and realistic completion date. Once we get the green light we usually begin work within 6-8 weeks.',
+    },
+    {
+      question:
+        'I’ve seen a new Kitchen / Bathroom I like. Do you offer an installation only service?',
+      answer:
+        'Sure. Arrange a no obligation site visit with our surveyor by calling 0121 441 5222. We can usually call out within 2 working days including Saturday. Site visit takes between 20-30 minutes depending on work required. Our representative will discuss your needs and explain anything you might not be sure about. We draw up a detailed plan of work required and costs breakdown. We will also include a guaranteed start date and realistic completion date. Once we get the green light we usually begin work within 6-8 weeks.',
+    },
+    {
+      question: 'Is it possible to get a rough estimate of costs by email?',
+      answer:
+        'Sure. Arrange a no obligation site visit with our surveyor by calling 0121 441 5222. We can usually call out within 2 working days including Saturday. Site visit takes between 20-30 minutes depending on work required. Our representative will discuss your needs and explain anything you might not be sure about. We draw up a detailed plan of work required and costs breakdown. We will also include a guaranteed start date and realistic completion date. Once we get the green light we usually begin work within 6-8 weeks.',
+    },
+    {
+      question: 'How do I request a quote for my project?',
+      answer:
+        'Sure. Arrange a no obligation site visit with our surveyor by calling 0121 441 5222. We can usually call out within 2 working days including Saturday. Site visit takes between 20-30 minutes depending on work required. Our representative will discuss your needs and explain anything you might not be sure about. We draw up a detailed plan of work required and costs breakdown. We will also include a guaranteed start date and realistic completion date. Once we get the green light we usually begin work within 6-8 weeks.',
+    },
+    {
+      question: 'Are you licensed and insured?',
+      answer:
+        'Sure. Arrange a no obligation site visit with our surveyor by calling 0121 441 5222. We can usually call out within 2 working days including Saturday. Site visit takes between 20-30 minutes depending on work required. Our representative will discuss your needs and explain anything you might not be sure about. We draw up a detailed plan of work required and costs breakdown. We will also include a guaranteed start date and realistic completion date. Once we get the green light we usually begin work within 6-8 weeks.',
+    },
+    {
+      question:
+        'I’ve never had my home extended before. Can you tell me what to expect?',
+      answer:
+        'Sure. Arrange a no obligation site visit with our surveyor by calling 0121 441 5222. We can usually call out within 2 working days including Saturday. Site visit takes between 20-30 minutes depending on work required. Our representative will discuss your needs and explain anything you might not be sure about. We draw up a detailed plan of work required and costs breakdown. We will also include a guaranteed start date and realistic completion date. Once we get the green light we usually begin work within 6-8 weeks.',
+    },
+  ];
   return (
     <Container>
       <StyledFaqSection>
         <div className="flex-container">
-          <div className="flex-item">
-            <p>Title</p>
-          </div>
-          <div className="flex-item">
-            <p>Title</p>
-          </div>
-          <div className="flex-item">
-            <p>Title</p>
-          </div>
-          <div className="flex-item">
-            <p>Title</p>
-          </div>
+          {faqs.map((faq, i) => (
+            <FaqItem key={i} question={faq.question} answer={faq.answer} />
+          ))}
         </div>
       </StyledFaqSection>
     </Container>
@@ -32,13 +58,5 @@ const StyledFaqSection = styled.section`
     display: flex;
     flex-direction: column;
     gap: 1.2rem;
-  }
-  .flex-item {
-    background: #ededed;
-    padding: 0 1rem;
-    border-radius: 0.3rem;
-  }
-  p {
-    font-weight: 600;
   }
 `;
---
+++ components/service-page-into-sections/FaqItem.js
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+import { AnimatePresence, motion } from 'framer-motion';
+import Chevron from '../../public/images/icons/chevron-down-black.png';
+import { useState } from 'react';
+import Image from 'next/image';
+export default function FaqItem({ question, answer }) {
+  const [expanded, setExpanded] = useState(false);
+  return (
+    <StyledFaqItem expanded={expanded} onClick={() => setExpanded(!expanded)}>
+      <div className="question">
+        <p> {question}</p>
+        <div className="chevron-container">
+          <Image src={Chevron} />
+        </div>
+      </div>
+      <AnimatePresence initial={false} mode="wait">
+        {expanded && (
+          <motion.div
+            animate={{
+              zIndex: 1,
+              transition: {
+                type: 'tween',
+                duration: 0.15,
+                ease: 'circOut',
+              },
+            }}
+            exit={{
+              zIndex: 0,
+              transition: {
+                type: 'tween',
+                duration: 0.15,
+                ease: 'circIn',
+              },
+            }}
+            className="answer"
+          >
+            <p> {answer}</p>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </StyledFaqItem>
+  );
+}
+const StyledFaqItem = styled.div`
+  background: #ededed;
+  padding: 1rem;
+  border-radius: 0.3rem;
+  cursor: pointer;
+  p {
+    margin: 0;
+  }
+  .question {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .chevron-container {
+      width: 1rem;
+      transform: ${(props) => (props.expanded ? 'rotate(180deg)' : '')};
+    }
+    p {
+      font-weight: 600;
+    }
+  }
+  .answer {
+    p {
+      margin-top: 1rem;
+    }
+  }
+`;
