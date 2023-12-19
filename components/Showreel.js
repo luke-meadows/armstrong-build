7d6cca45be3d6981de0fad3acbd33d7d13fc0a1e
luke-meadows <lukemeadowsdev@gmail.com>
2023-12-19 14:39:05 +0000

    final changes

--- components/Showreel.js
+++ components/Showreel.js
@@ -22,3 +22,44 @@ export default function Showreel({ id = '871787555', coupled = false }) {
     </Container>
   );
 }
+const StyledShowreel = styled.div`
+  position: relative;
+  border-radius: 0.3rem;
+  border-bottom-left-radius: ${(props) => (props.coupled ? '0rem' : '0.3rem')};
+  border-bottom-right-radius: ${(props) => (props.coupled ? '0rem' : '0.3rem')};
+  height: 15rem;
+  height: 40rem;
+  overflow: hidden;
+  margin-bottom: ${(props) => (props.coupled ? '0rem' : '4rem')};
+  background: #000;
+  z-index: 1;
+  iframe {
+    width: 100%;
+    min-height: 112%;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 3;
+  }
+  .logo-container {
+    z-index: 4;
+    position: absolute;
+    bottom: 1.8rem;
+    right: 1.8rem;
+  }
+  @media only screen and (max-width: 1235px) {
+    height: 15rem;
+    height: 25rem;
+  }
+  @media only screen and (max-width: 900px) {
+    height: 26rem;
+    iframe {
+      display: none;
+    }
+    .logo-container {
+      bottom: 1rem;
+      right: 1rem;
+    }
+  }
+`;
